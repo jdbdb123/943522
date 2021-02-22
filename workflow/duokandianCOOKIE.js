d158530a95cfe8fa0defeddceffb383a3c5d014c
Xin-Qiang <79135038+Xin-Qiang@users.noreply.github.com>
2021-02-22 21:57:39 +0800

    Update duokandianCOOKIE.js

--- workflow/duokandianCOOKIE.js
+++ workflow/duokandianCOOKIE.js
@@ -1,6 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let duokandianbodyVal = ``
-let duokandianvideobodyVal = ``
+let duokandianbodyVal = `token=96c3511abb1d1aed0959b53eb6f2fa45`
+let duokandianvideobodyVal = `params=AZu0wE5uC7S1gGL%2BsnrWam1ywkuMR1LYjbE2mg/k%2B6mpTex8ZfsexrfklvHD8U/6fZT0Z5PJgi2UEQPzOmLE3%2BLn9hN8snqiXnjzJ64Izh5eYzgR0Rn%2B9lpsfaJGTfUm`
 
 
 let duokandiancookie = {
