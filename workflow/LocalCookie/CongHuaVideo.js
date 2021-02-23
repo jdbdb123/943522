3bcfd37a4a022ff86bc50b95b6c97865d79dc623
Xin-Qiang <79135038+Xin-Qiang@users.noreply.github.com>
2021-02-23 21:59:38 +0800

    Update CongHuaVideo.js

--- workflow/LocalCookie/CongHuaVideo.js
+++ workflow/LocalCookie/CongHuaVideo.js
@@ -43,11 +43,11 @@ if ($.isNode()) {
 async function showmsg() {
   if (notifyInterval == 1) {
     if ($.isNode()) {
-      if ((hour == 8 && minute <= 20) || (hour == 12 && minute <= 20) || (hour == 23 && minute <= 20)) {
+      if ((hour == 0 && minute <= 12) || (hour == 4 && minute <= 20) || (hour == 15 && minute <= 20)) {
         await notify.sendNotify($.name, tz)
       }
     } else {
-      if ((hour == 8 && minute <= 20) || (hour == 12 && minute <= 20) || (hour == 23 && minute <= 20)) {
+      if ((hour == 0 && minute <= 12) || (hour == 4 && minute <= 20) || (hour == 15 && minute <= 20)) {
         $.msg(msgstyle, '', tz);
       }
     }
@@ -224,12 +224,12 @@ if ($.isNode()) {
     );
   }
 
-  if (hour == 9 || hour == 11 || hour == 14) {
+  if (hour == 1 || hour == 3 || hour == 6) {
     await videoread(); //自动刷视频
-  }else if (hour == 12) {
+  }else if (hour == 4) {
     console.log(`\n✅ 执行自行助力任务`)
     await callback();
-  }else if (hour <= 18) {
+  }else if (hour <= 10) {
     console.log(`\n✅ 打印任务状态清单`)
     await taskcenter(); //任务中心
     console.log(`\n✅ 执行时段奖励任务`)
