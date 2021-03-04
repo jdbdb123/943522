b5089670ad4e64a40d83f10b0c69e7258283c793
XQ0120 <79135038+XQ0120@users.noreply.github.com>
2021-03-04 17:50:08 +0800

    Update TomatoesLook.js

--- workflow/TomatoesLook.js
+++ workflow/TomatoesLook.js
@@ -7,7 +7,7 @@ let fqkkhd = $.getdata('fqkkhd')
 let fqkey = ''
 let fqkkxh = ($.getval('fqkkxh') || '20');  // 此处修改循环次数，默认一百
 let fqtx = ($.getval('fqtx') || '100');  // 此处修改提现金额，0.1元等于10，默认为提现一元，也就是100
-let max = 180
+let max = 90
 let min = 37
 
 if ($.isNode()) {
