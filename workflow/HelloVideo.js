bf52ceabf23b9d3cc796f4f42e0d6fb3a59c15e8
XQ0120 <79135038+XQ0120@users.noreply.github.com>
2021-03-14 16:20:16 +0800

    Update HelloVideo.js

--- workflow/HelloVideo.js
+++ workflow/HelloVideo.js
@@ -83,7 +83,7 @@ let url = {
            
     const result = JSON.parse(data)
         if(result.data !== 0){
-        console.log('\n哈喽短视频[领取视频红包]回执:成功🌝 \n获得视频奖励: '+result.data+'等待20秒继续领取')
+        console.log('\n哈喽短视频[领取视频红包]回执:成功🌝 \n获得视频奖励: '+result.data+',等待20秒继续领取')
            await $.wait(20000);
            await hldspsp();
        
